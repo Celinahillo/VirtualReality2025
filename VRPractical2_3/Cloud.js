d48e1bfce6403b3d12b72800c3892869a8ad8fb4
Celinahillo <celinah5@nycstudents.net>
2025-10-22 16:30:25 +0000

    .....

---
+++ VRPractical2_3/Cloud.js
@@ -0,0 +1,28 @@
+class Cloud{
+  constructor(x,y,z){
+    this.cloud = document.createElement("a-entity");
+
+  let sphere1 = document.createElement("a-sphere");
+  sphere1.setAttribute("color","white");
+  sphere1.setAttribute("position","0 2 0");
+  sphere1.setAttribute("height","2");
+  this.cloud.append( sphere1 );
+
+  let sphere2 = document.createElement("a-sphere");
+  sphere2.setAttribute("color","white");
+  sphere2.setAttribute("position","1 2.5 0");
+  sphere2.setAttribute("height","2");
+  this.cloud.append( sphere2 );
+
+  let sphere3 = document.createElement("a-sphere");
+  sphere3.setAttribute("color","white");
+  sphere3.setAttribute("position","2 2 0");
+  sphere3.setAttribute("height","2");
+  this.cloud.append( sphere3 );
+
+  this.cloud.setAttribute("position", {x:x, y:y, z:z});
+  scene.append( this.cloud )
+
+
+  }
+}
