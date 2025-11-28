526584f8645754c0a1d61b7fef214de0382a0d5e
Celinahillo <celinah5@nycstudents.net>
2025-11-28 01:16:04 +0000

    ok...

--- VR_Unit3/VRPractical3_2/script.js
+++ VR_Unit3/VRPractical3_2/script.js
@@ -1,26 +1,60 @@
-let rocket;
+let rocket, ufo, tree;
 
-window.addEventListener("DOMContentLoaded", function () {
+window.addEventListener("DOMContentLoaded", function() {
   rocket = document.getElementById("rocket");
 
-  rocket.y = 0;
-  rocket.dy = 0.05;
+  rocket.y = 0.5;
+  rocket.dy = 0.1;
   rocket.launch = false;
 
-  // Start launch when mouse goes over rocket
-  rocket.addEventListener("mouseenter", function () {
+  rocket.addEventListener("mouseenter", function() {
     rocket.launch = true;
-    console.log("Rocket hover detected!");
   });
+
+  ufo = document.getElementById("ufo");
+  ufo.y = 5;          // starting height
+  ufo.dy = -0.1;      // speed falling down
+
+  ufo.addEventListener("mouseenter", function() {
+    ufo.hit = true;
+  });
+  ufo.addEventListener("mouseleave", function() {
+    ufo.hit = false;
+  });
+
+  // ====== Tree Setup ======
+tree = document.getElementById("tree");
+tree.rotationZ = 0;      // initial rotation
+tree.dr = 1;              // rotation speed
+tree.fall = false;
+
+tree.addEventListener("click", function() {
+  tree.fall = true;
+});
+
 
   loop();
 });
 
 function loop() {
-  if (rocket.launch) {
+  if(rocket.launch){
     rocket.y += rocket.dy;
-    rocket.setAttribute("position", { x: 0, y: rocket.y, z: -6 });
+    rocket.setAttribute("position", {x: 0, y: rocket.y, z: -3});
   }
 
-  requestAnimationFrame(loop);
+   if (ufo.hit) {
+    ufo.y += ufo.dy;
+    ufo.setAttribute("position", {x: 2, y: ufo.y, z: -3});
+  }
+
+  if(tree.fall){
+  tree.rotationZ += tree.dr;   // rotate the tree
+  tree.setAttribute("rotation", {x:0, y:0, z: tree.rotationZ});
+if(tree.rotationZ >= 90){
+    tree.rotationZ = 90;
+    tree.fall = false;
+  }
+    }
+ 
+  window.requestAnimationFrame(loop);
 }
